ae27a6be0fd85eac15ce78b4d854671f418aef3f
Cody Taylor <codemister99@yahoo.com>
2021-06-18 13:00:49 -0400

    Print the board.

--- Program.fs
+++ Program.fs
@@ -22,6 +22,9 @@ module Cell =
 
 [<EntryPoint>]
 let main argv =
+    let sudokuBoardText = File.ReadAllText "sudoku.txt"
+    do printfn "%s" sudokuBoardText
+
     let token = 1 |> Cell.create |> Cell.render
     do printfn "Token: %A" token
 
